e653c7307b1f095ef4dba573c90f5ac9c4e09d26
mirkamalice <mirkamalice@gmail.com>
2021-03-13 07:30:35 +0330

    add load more button

--- app/src/main/java/com/mob/developer/hw1/MainActivity.java
+++ app/src/main/java/com/mob/developer/hw1/MainActivity.java
@@ -15,15 +15,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -38,8 +33,13 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Coin> coinArrayList;
     private RecyclerView rvCoins;
+    private Button loadMore;
     private Adapter.OnItemClickListener listener;
     private Handler handlerThread;
+    private int lastCoin = 1;
+    public final int LOAD_FROM_API = 1;
+    public final int FIRST_LOAD_CACHE = 2;
+    public final int REFRESH_DATA = 3;
 
 
     @SuppressLint("HandlerLeak")
@@ -49,17 +49,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        generateData();
+        generateData(lastCoin,10);
+
+        loadMore.setOnClickListener(view -> {
+            generateData(lastCoin,5);
+        });
 
 
         /*
-         * replace generateData() with:
-         * load data from cache and show loading
-         * get data from api in new thread and fetch it in UI
+         * add load data from cache
          * */
-
-
-
 
         /*
          * set onClickListener for 'load more' button
@@ -69,27 +68,23 @@ public class MainActivity extends AppCompatActivity {
 
         handlerThread = new Handler() {
             @Override
-            public void handleMessage(Message m) {
-                super.handleMessage(m);
-                if (m.what == 1) {
-                    if (m.arg1 == 1) {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (msg.what == LOAD_FROM_API) {
+                    if (msg.arg1 == 1) {
                         coinArrayList = Coin.allCoins;
                         setData();
                     } else {
                         Log.v("mylog", "error in handle msg");
-
                     }
-
                 }
             }
         };
 
 
-
-
     }
 
-    private void showLoading(){
+    private void showLoading() {
         ProgressBar progressBar = findViewById(R.id.progressBar1);
         progressBar.setVisibility(View.VISIBLE);
         Button load = findViewById(R.id.load);
@@ -97,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
         load.setVisibility(View.GONE);
         refresh.setVisibility(View.GONE);
     }
-    private void hideLoading(){
+
+    private void hideLoading() {
         ProgressBar progressBar = findViewById(R.id.progressBar1);
         progressBar.setVisibility(View.GONE);
         Button load = findViewById(R.id.load);
@@ -109,17 +105,19 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         coinArrayList = new ArrayList<>();
         rvCoins = findViewById(R.id.rv_coins);
+        loadMore = findViewById(R.id.load);
     }
 
-    private void generateData() {
+    private void generateData(int from,int limit) {
         showLoading();
         Thread threadGetData = new Thread(new Runnable() {
             @Override
             public void run() {
                 Message message = new Message();
-                message.what = 1;
+                message.what = LOAD_FROM_API;
                 try {
-                    boolean temp = getDataFromCoinMarketCap(1, 5);
+                    boolean temp = getDataFromCoinMarketCap(from, limit);
+                    lastCoin+=5;
                     if (temp) {
                         message.arg1 = 1;
                     } else {
@@ -139,7 +137,6 @@ public class MainActivity extends AppCompatActivity {
         listener = new Adapter.OnItemClickListener() {
             @Override
             public void onItemClick(Coin item) {
-//                Toast.makeText(getApplicationContext(),item.getName(),Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, CoinOHLC.class);
                 intent.putExtra("name", item.getName());
                 intent.putExtra("abbr", item.getSymbol());
@@ -148,7 +145,6 @@ public class MainActivity extends AppCompatActivity {
         };
         rvCoins.setAdapter(new Adapter(coinArrayList, listener));
         hideLoading();
-//        rvCoins.setAdapter(new Adapter(this, coinArrayList));
     }
 
 
@@ -170,30 +166,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             return false;
         }
-//        okHttpClient.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-//                Log.v("mylog", e.getMessage());
-//            }
-//
-//            @Override
-//            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-//                if (!response.isSuccessful()) {
-//                    throw new IOException("Unexpected code " + response);
-//                } else {
-//                    //extractCandlesFromResponse(response.body().string(), description);
-//                    String responseText = response.body().string();
-//
-//                    String first = "},\"data\":[";
-//                    int location = responseText.indexOf(first);
-//                    location+= 9;
-//                    responseText = responseText.substring(location,responseText.length()-1);
-//
-//                    Log.v("mylog", responseText);
-//                    Coin.convertJsonToCoins(responseText);
-//                }
-//            }
-//        });
     }
 
 
